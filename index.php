8c54200a4552b4387ecf6315b689816ceefee611
PD3v3sa <103313276+PD3v3sa@users.noreply.github.com>
2026-05-18 16:12:10 +0200

    Remove unused variables and HTML echo statements

--- index.php
+++ index.php
@@ -2,13 +2,3 @@
 
 echo "Hello World!";
  
-        $curso = "Microsoft Azure";
-        $tema = "Configurando nuestro primer App Service";
-        $plataforma = "Azure Portal";
-        $servicio = "App Service";
-        $lenguaje = "PHP";
-    
-
-    <h1> echo $curso; </h1>
-
-    <h2> echo $tema; </h2>
