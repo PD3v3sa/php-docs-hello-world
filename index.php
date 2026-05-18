cb915d18cf3aabfec91bc0707171304eadcd55f3
PD3v3sa <103313276+PD3v3sa@users.noreply.github.com>
2026-05-18 16:34:23 +0200

    Add HTML structure to index.php
    Updated index.php to include HTML structure.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo "Hello World! Cefire";
- 
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso de Azure - App Service</title> 
+</head>
+<body>
+ <?php
+ echo "Hello World! Cefire";
+</body>
+</html>
