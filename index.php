1b3f9f3067443a7d437eb615bc24a33817dac298
PD3v3sa <103313276+PD3v3sa@users.noreply.github.com>
2026-05-18 16:09:47 +0200

    Add course details to index.php
    Added variables for course details and displayed them in HTML.

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 
 echo "Hello World!";
+ <?php
+        $curso = "Microsoft Azure";
+        $tema = "Configurando nuestro primer App Service";
+        $plataforma = "Azure Portal";
+        $servicio = "App Service";
+        $lenguaje = "PHP";
+    ?>
 
+    <h1><?php echo $curso; ?></h1>
+
+    <h2><?php echo $tema; ?></h2>
